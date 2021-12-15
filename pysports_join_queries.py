caa6edae9c3f1f4756c2a270333664b78b09fd80
yaffagoldberger <92687366+yaffagoldberger@users.noreply.github.com>
2021-12-14 23:32:12 -0500

    Create pysports_join_queries.py

---
+++ pysports_join_queries.py
@@ -0,0 +1,32 @@
+
+PLAYER RECORDS
+
+PLAYER ID: 01
+First Name: Mike
+Last Name: Jord
+Team Name: Team YouGo
+
+PLAYER ID: 02
+First Name: Babe
+Last Name: O'Connor
+Team Name: Team YouGo
+
+PLAYER ID: 03
+First Name: Sammy
+Last Name: Hilfiger
+Team Name: Team YouGo
+
+PLAYER ID: 04
+First Name: James
+Last Name: Brandi
+Team Name: Team WeGotThis
+
+PLAYER ID: 05
+First Name: Johny
+Last Name: Smith
+Team Name: Team WeGotThis
+
+PLAYER ID: 06
+First Name: Genesha
+Last Name: Klein
+Team Name: Team WeGotThis
